6613f5aaad7fc77b992ae9f3eaf0f3933cdb8cbd
LSH <dltpgns97@gmail.com>
2022-04-16 20:55:59 +0900

    add details

--- src/routes/Detail.tsx
+++ src/routes/Detail.tsx
@@ -1,17 +1,38 @@
-import { Link, useParams } from "react-router-dom";
+import {  useParams } from "react-router-dom";
 import Head from "../components/Head";
+import GetDetails from "../data/GetDetails";
 
 function Detail(){
     const {id} = useParams();
 
-    //GetDetails(id);
+    if(id){
+        const [titles,texts] = GetDetails(id);
 
-    return(
-        <div>
-            <Head />
-            <p>{id}에 관련한 내용들</p>
-        </div>
-    );
+        return(
+            <div>
+                <Head />
+                <p>{id}에 관련한 내용들</p>
+                {titles.map((title,index)=>(
+                    <div>
+                        <h1>{title}</h1>
+                        <span>{texts[index]}</span>
+                    </div>
+                ))}
+            </div>
+        );
+    }
+    else{
+        return(
+            <div>
+                <Head />
+                <p>{id}에 관련한 내용들</p>
+                <h1>Get Data Fail</h1>
+            </div>
+        );
+    }
+    
+
+    
 }
 
 export default Detail;
--- src/routes/ETC.tsx
+++ src/routes/ETC.tsx
@@ -1,12 +1,19 @@
 import Head from "../components/Head";
+import GetDetails from "../data/GetDetails";
 
 function ETC(){
 
-    //GetDetails('etc');
+    const [titles, texts] = GetDetails('etc');
     return(
         <div>
             <Head />
-            기타 부분들
+            <span>기타 부분들</span>
+            {titles.map((title,index)=>(
+                    <div>
+                        <h1>{title}</h1>
+                        <span>{texts[index]}</span>
+                    </div>
+                ))}
         </div>
     );
 }
